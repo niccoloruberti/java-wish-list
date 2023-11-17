03a33376936c4288fbae7ce249ab4f56bab37e26
niccoloruberti <nikiruberti@gmail.com>
2023-11-17 12:20:23 +0100

    es 1 base completato

--- src/org/lessons/java/christmas/WishList.java
+++ src/org/lessons/java/christmas/WishList.java
@@ -1,9 +1,41 @@
 package org.lessons.java.christmas;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WishList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+		ArrayList<String> gifts = new ArrayList<>();
+		
+		while (true) {
+			
+			System.out.println("Vuoi aggiungere un regalo alla lista? si/no");
+			
+			String ansUser = in.nextLine();
+			
+			if (ansUser.equals("si")) {
+				
+				System.out.println("Inserisci il regalo da aggiungere alla lista: ");
+				
+				String gift = in.nextLine();
+				
+				gifts.add(gift);
+				
+				System.out.println("lunghezza della lista: " + gifts.size());
+				
+			} else {
+				
+				break;
+			}
+		}
+		
+		in.close();
+		
+		System.out.println(gifts);
 
 	}
 
